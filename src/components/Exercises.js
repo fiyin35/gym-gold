67d6a94ced7b7a309d2389c47a65c50a1f0d98c9
Fiyinfolu Odeleke <fiyinfolu.odeleke@longbridgetech.com>
2022-06-25 23:17:50 +0100

    stil work in progress

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Typography, Box, Stack} from '@mui/material';
+import { Pagination } from '@mui/material/Pagination';
+import { exerciseOptions, fetchData} from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, setBodyPart}) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9; 
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;;
+
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth'})
+  }
   return (
-    <div>
-      Exercises
-    </div>
+    <Box 
+    id="exercises"
+    sx={{ mt: { lg: '110px' }}}
+    mt="50px"
+    p="20px"
+    >
+
+      <Typography variant="h3" mb="46px"> Showing result</Typography>     
+      <Stack direction="row"
+        sx={{ gap: { lg: '110px', xs: '50px'}}}
+        flexWrap="wrap" justifyContent="center">
+           {currentExercises.map((exercise, index) => (
+             <ExerciseCard key={index} exercise={exercise}/>
+           ))}
+        </Stack>
+        <Stack mt="100px" alignItems="center"> 
+            {exercises.length > 9 && (
+                <Pagination 
+                color="standard" 
+                shape="rounded" 
+                defaultPage={1} 
+                count={Math.ceil(exercises.length / exercisesPerPage)}
+                page={currentPage}
+                onChange={paginate}
+                size="large"/>
+            )}
+        </Stack>
+    </Box>
   )
 }
 
-export default Exercises
+export default Exercises;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,13 +1,23 @@
 import React, {useEffect, useState} from 'react'
 import { Box, Button, Stack, TextField, Typography} from '@mui/material'
 import { exerciseOptions, fetchData} from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-
-const SearchExercises = () => {
+const SearchExercises = ({setBodyPart, bodyPart, setExercises}) => {
   const [search, setSearch] = useState('');
-  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([])
 
   const URL = 'https://exercisedb.p.rapidapi.com/exercises';
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+       const bodyPartsData = await fetchData(URL, exerciseOptions)
+
+       setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExercisesData();
+}, [])
 
   const handleSearch = async () => {
       if (search) {
@@ -71,6 +81,12 @@ const SearchExercises = () => {
             > 
               Search
             </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
+            <HorizontalScrollbar 
+            data={bodyParts} 
+            bodyPart={bodyPart} 
+            setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
